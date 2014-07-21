25fcc2e6736871416d946671d4b39b18ea113294
Jung Woojoo <juniversevivi@gmail.com>
2014-07-21 16:47:09 +0900

    step 7-1

--- flaskr.py
+++ flaskr.py
@@ -1,45 +1,69 @@
+
 import os
-import sqlite3
-from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from sqlite3 import dbapi2 as sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
 
-app = Flask(_name_)
-app.config.from_object(_name_)
 
+# create our little application :)
+app = Flask(__name__)
+
+# Load default config and override config from an environment variable
 app.config.update(dict(
-	DATABASE=os.path.join(app.root_path, 'flaskr.db'),
-	DEBUG=True,
-	SECRET_KEY='development key',
-	USERNAME='admin',
-	PASSWORD='default'
+    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+    DEBUG=True,
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
 ))
-app.config.from_envvar('')
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
-def connet_db():
-	rv = sqlite3.connect(app.config['DATABASE'])
-	rv.row_factory = squlite3.row_factory
-	return rv
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
 
 def init_db():
-	with app.app_context():
-		db = get_db()
-		with app.open_resource('schema.sql', mode ='r') as f:
-			db.
+    """Initializes the database."""
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+
+@app.cli.command('initdb')
+def initdb_command():
+    """Creates the database tables."""
+    init_db()
+    print('Initialized the database.')
 
 
 def get_db():
-	if not hasattr(g, 'sqlite_db'):
-		g.sqlite_db = connect_db()
-	return g.sqlite_db
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
 
-def close_db(error) :
-	if hasattr(g, 'sqlite_db'):
-		g.sqlite_db.close()
 
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+
+@app.route('/')
 def show_entries():
     db = get_db()
     cur = db.execute('select title, text from entries order by id desc')
     entries = cur.fetchall()
     return render_template('show_entries.html', entries=entries)
+
 
 @app.route('/add', methods=['POST'])
 def add_entry():
@@ -47,10 +71,11 @@ def add_entry():
         abort(401)
     db = get_db()
     db.execute('insert into entries (title, text) values (?, ?)',
-                 [request.form['title'], request.form['text']])
+               [request.form['title'], request.form['text']])
     db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -72,6 +97,4 @@ def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
     return redirect(url_for('show_entries'))
-
-
 
